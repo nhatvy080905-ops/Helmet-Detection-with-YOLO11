5fdb7b15424c0fe6b4d7c702a62f53e17805c108
Truong Nhat Vy <nhatvy.080905@gmail.com>
2026-06-05 10:27:20 +0700

    Update project

--- Folder_code/check.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="API_KEY_CUA_BAN")
-project = rf.workspace("cdis-naxqr").project("helmet-detection-bon9f")
-dataset = project.version(1).download("yolov8")
